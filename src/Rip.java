b15b5af3620c91952e176bebb15af282e43af51c
Pablo <parope.2@gmail.com>
2016-04-15 19:17:38 +0200

    Empezamos

---
+++ src/Rip.java
@@ -0,0 +1,30 @@
+public class Rip {
+
+	/*
+	 * 1º Procesar argumento de entrada, u obtener la IP del ¿interna?
+	 * - interfaz eth0 IP asociada
+	 * 2º ripconfig A.B.C.D ¿topo?
+	 * - leer y procesar este fichero
+	 * 172.19.45.9:4444 <-- IPs vecinos + puerto
+	 * ------
+	 * ------
+	 * I.J.K.L/25 <-- Subred finalizada
+	 * 3º Crear sockets.[UDP]
+	 * - Configurar un tier en el socket
+	 * DatagramSocket
+	 * do{
+	 * try{
+	 * receive(); //Proceso paquete, Actualizo tabla;
+	 * } catch{ //Gestiona el vencimiento del temporizador
+	 * //Envía update a vecino
+	 * //¿Reset? temporizador
+	 * }
+	 * } while (true);
+	 * 
+	 * Bellman-Ford!!!
+	 */
+
+	public static void main(String[] args) {
+	}
+
+}
